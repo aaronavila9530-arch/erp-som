b1ad9c9e5a50b7d19b77399d774f89e28a57dfe6
aaronavila9530-arch <aaron.avila9530@gmail.com>
2026-01-14 16:20:52 -0600

    HHRR

--- backend_api/routers/hr_events.py
+++ backend_api/routers/hr_events.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Body
 from psycopg2.extras import RealDictCursor
 from datetime import date, datetime
 
